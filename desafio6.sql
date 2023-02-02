1073c73981f9fdfbd950e24466d2d315d55a9912
EduardoL-s <eduardoluizmm@hotmail.com>
2023-02-02 17:55:13 -0300

    [Desafio 6] Exibição do menor e o maior valor cobrado pelos planos, além de valor médio e total arrecadado pela empresa através dos planos - Concluído

---
+++ desafio6.sql
@@ -0,0 +1,7 @@
+SELECT
+	MIN(plano.plano_valor) AS faturamento_minimo,
+    MAX(plano.plano_valor) AS faturamento_maximo,
+    ROUND(AVG(plano.plano_valor), 2) AS faturamento_medio,
+    SUM(plano.plano_valor) AS faturamento_total
+FROM SpotifyClone.usuario
+INNER JOIN SpotifyClone.plano ON usuario.plano_id = plano.plano_id;
